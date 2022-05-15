93f520404e682d964bf1ba0f1e35ae26e7a94613
makaota <74915165+makaota@users.noreply.github.com>
2022-05-15 15:49:35 +0200

    exploring js variables

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-console.log('Hello')
-console.log(123)
-console.log(true)
-var message = 'This is a new message'
+var name = "Alan Smith"
+console.log(name)
+name = "John William"
+console.log(name)
+
+var message
 console.log(message)
-console.log([1, 2, 3])
-console.log({
-    name: 'John',
-    age: 18
-})
-console.table({
-    name: 'John',
-    age: 18
-})
-console.error("Error!!")
-console.warn("Warning!!")
-console.clear()
-console.time('time')
-console.log('timing')
-console.log('timing')
-console.log('timing')
-console.log('timing')
-console.log('timing')
-console.log('timing')
-console.timeEnd('time')
+message = "New message"
+console.log(message)
+
+//letters, number, _ , $
+//X start with numbers
+var $name = 'Some name'
+
+var firstName //CamelCase
+var last_name //underscore
+var FirstName //PascalCase
