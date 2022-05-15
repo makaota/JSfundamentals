3bc3dc59bb29ddab1dc0148c88a6e9a673337641
makaota <74915165+makaota@users.noreply.github.com>
2022-05-15 16:12:28 +0200

    understanding let and const keywords

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('setup')
+// let name
+// name = "Alan"
+// console.log(name)
+// name = "John"
+// console.log(name)
+
+// const name = "Alan"
+// name = "John"
+
+// const user = {
+//     name: 'Alan',
+//     age: 30
+// }
+// console.log(user)
+// user.name = "John"
+// console.log(user)
+
+const arr = []
+console.log(arr)
+arr.push(3, 4)
+console.log(arr)
